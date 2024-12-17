3ca1bb11bd3f0ddb27a3ef80d6edd58018f50d15
Karol Pichurski <karolp8901@gmail.com>
2024-12-18 00:02:42 +0100

    Lab2: zad: 1,2

---
+++ wsb-jpa1/src/main/java/com/jpacourse/mapper/PatientMapper.java
@@ -0,0 +1,53 @@
+package com.jpacourse.mapper;
+
+import com.jpacourse.dto.PatientTO;
+import com.jpacourse.persistence.entity.PatientEntity;
+
+public final class PatientMapper
+{
+    public static PatientTO mapToTO(final PatientEntity patientEntity)
+    {
+        if (patientEntity == null)
+        {
+            return null;
+        }
+
+        final PatientTO patientTO = new PatientTO();
+        patientTO.setId(patientEntity.getId());
+        patientTO.setFirstName(patientEntity.getFirstName());
+        patientTO.setLastName(patientEntity.getLastName());
+        patientTO.setTelephoneNumber(patientEntity.getTelephoneNumber());
+        patientTO.setEmail(patientEntity.getEmail());
+        patientTO.setPatientNumber(patientEntity.getPatientNumber());
+        patientTO.setDateOfBirth(patientEntity.getDateOfBirth());
+        patientTO.setGender(patientEntity.getGender());
+
+        patientTO.setAddress(patientEntity.getAddress());
+        patientTO.setVisits(patientEntity.getVisits());
+
+        return patientTO;
+    }
+
+    public static PatientEntity mapToEntity(final PatientTO patientTO)
+    {
+        if(patientTO == null)
+        {
+            return null;
+        }
+
+        PatientEntity patientEntity = new PatientEntity();
+        patientEntity.setId(patientTO.getId());
+        patientEntity.setFirstName(patientTO.getFirstName());
+        patientEntity.setLastName(patientTO.getLastName());
+        patientEntity.setTelephoneNumber(patientTO.getTelephoneNumber());
+        patientEntity.setEmail(patientTO.getEmail());
+        patientEntity.setPatientNumber(patientTO.getPatientNumber());
+        patientEntity.setDateOfBirth(patientTO.getDateOfBirth());
+        patientEntity.setGender(patientTO.getGender());
+
+        patientEntity.setAddress(patientTO.getAddress());
+        patientEntity.setVisits(patientTO.getVisits());
+
+        return patientEntity;
+    }
+}
